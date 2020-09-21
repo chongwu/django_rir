5d0d5399ead52074f6f58ba131f6966b0bdb53ab
Denis Podlasenko <jokerd1@gmail.com>
2020-09-22 02:23:34 +0300

    upload questionnaires feature

--- staff/urls.py
+++ staff/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.person_list, name='person_list'),
     path('<int:person_id>/', views.person_detail, name='person_detail'),
     path('<int:person_id>/questionnaire/', views.create_questionnaire, name='person_create_questionnaire'),
+    path('<int:person_id>/questionnaire/upload', views.upload_questionnaire, name='person_upload_questionnaire'),
+    path('<int:person_id>/questionnaire/download', views.download_questionnaire, name='person_download_questionnaire'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
+from django.utils.encoding import smart_str
 from .models import Person
+from questionnaires.models import Questionnaire, QuestionnaireRow
 from itertools import groupby
 from operator import attrgetter
+from openpyxl import Workbook, load_workbook
+from openpyxl.writer.excel import save_virtual_workbook
+from tempfile import NamedTemporaryFile
+import os
 
 
 # Create your views here.
@@ -23,3 +29,42 @@ def create_questionnaire(request, person_id):
     person = get_object_or_404(Person, tab_number=person_id)
     categories = person.position.first().competence_category.all()
     return render(request, 'staff/person/questionnaire_form.html', {'person': person, 'categories': categories})
+
+
+def upload_questionnaire(request, person_id):
+    person = get_object_or_404(Person, tab_number=person_id)
+    if request.method == 'POST':
+        files = request.FILES['questionnaire']
+        book = load_workbook(files.file)
+        sheet = book.active
+        questionnaire = Questionnaire(person=person)
+        questionnaire.save()
+        rows = []
+        for i in range(1, sheet.max_row + 1):
+            rows.append(QuestionnaireRow(
+                questionnaire=questionnaire,
+                competence_id=sheet.cell(row=i, column=1).value,
+                competence_val=sheet.cell(row=i, column=3).value,
+            ))
+        QuestionnaireRow.objects.bulk_create(rows)
+    return redirect(person)
+
+
+def download_questionnaire(request, person_id):
+    person = get_object_or_404(Person, tab_number=person_id)
+    categories = person.position.first().competence_category.all()
+    with NamedTemporaryFile(mode='w', delete=False) as f:
+        book = Workbook()
+        sheet = book.active
+        i = 1
+        for category in categories:
+            for competence in category.competencies.all():
+                sheet.cell(row=i, column=1).value = competence.id
+                sheet.cell(row=i, column=2).value = competence.name
+                i += 1
+        book.save(f.name)
+        book.close()
+        file_name = os.path.basename(f.name)
+        response = HttpResponse(content=save_virtual_workbook(book), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(file_name + '.xlsx')
+    return response
