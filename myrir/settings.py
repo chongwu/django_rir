0d56717d60d2c247fe3cfedcb638e9ee0a793cf3
Denis Podlasenko <jokerd1@gmail.com>
2021-01-23 23:22:24 +0300

    add admin email

--- myrir/settings.py
+++ myrir/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = '^67h(1+poj8co^w86^!adudz91&t(rg#^b9287tn3tsj5x@=ru'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['desolate-gorge-38482.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ggstfugg@gmail.com' #С которого будет приходить сообщение
+EMAIL_HOST_PASSWORD = 'ghjcnjzobr'
+EMAIL_PORT = 587
 
 ADMINS = [('Denis', 'jokerd1@gmail.com')]
 
@@ -141,57 +147,5 @@ import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'formatters': {
-        'django.server': {
-            '()': 'django.utils.log.ServerFormatter',
-            'format': '[%(server_time)s] %(message)s',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-        'console_debug_false': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'logging.StreamHandler',
-        },
-        'django.server': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'django.server',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'console_debug_false', 'mail_admins'],
-            'level': 'INFO',
-        },
-        'django.server': {
-            'handlers': ['django.server'],
-            'level': 'INFO',
-            'propagate': False,
-        }
-    }
-}
 
 django_heroku.settings(locals())
